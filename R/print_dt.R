364a36f20fdf1e3365310004c95d9ebddeb0d56f
Meera Patel <patelmeeray@gmail.com>
2021-01-03 14:27:34 -0500

    add print_dt function and template using it

---
+++ R/print_dt.R
@@ -0,0 +1,21 @@
+#' @title
+#' Print a Downloadable Table
+#'
+#' @description
+#' Print a data table in a R Markdown document as described in \url{https://martinctc.github.io/blog/vignette-downloadable-tables-in-rmarkdown-with-the-dt-package/}
+#'
+#' @rdname print_dt
+#' @export
+#' @importFrom DT datatable
+
+
+
+print_dt <-
+    function(x){
+    DT::datatable(x,
+                  extensions = 'Buttons',
+                  options = list(dom = 'Blfrtip',
+                                 buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+                                 lengthMenu = list(c(10,25,50,-1),
+                                                   c(10,25,50,"All"))))
+  }
